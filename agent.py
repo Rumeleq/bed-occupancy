506247037a0dd8fe3c6d0114b33730aa80f6f941
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-05-14 15:54:23 +0200

    add retrieving information about user's confirmation

--- agent.py
+++ agent.py
@@ -1,3 +1,4 @@
+import json
 import os
 import signal
 import sys
@@ -6,6 +7,7 @@ from dotenv import load_dotenv
 from elevenlabs.client import ElevenLabs
 from elevenlabs.conversational_ai.conversation import Conversation, ConversationInitiationData
 from elevenlabs.conversational_ai.default_audio_interface import DefaultAudioInterface
+from elevenlabs.core import RequestOptions
 
 load_dotenv()
 
@@ -58,6 +60,38 @@ try:
     signal.signal(signal.SIGINT, lambda sig, frame: conversation.end_session())
     conversation_id = conversation.wait_for_session_end()
     print(f"Conversation ID: {conversation_id}")
+
+    # Retrieve conversation data using the conversation ID
+    # conversation_data = client.conversational_ai.get_conversation_data(conversation_id=conversation_id)
+    conversation_data = client.conversational_ai.get_conversation(conversation_id=conversation_id)
+    # print(conversation_data.analysis.data_collection_results['consent_to_change_the_date'])
+
+    # with open("conversation.json", "w") as conversation_file:
+    #     conversation_file.write(conversation_data.analysis.json())
+    #
+    # with open("conversation.json", "r") as file:
+    #     data = json.load(file)
+    #
+    # consent_value = (data.get("data_collection_results", { })
+    #                  .get("consent_to_change_the_date", { })
+    #                  .get("value", None))
+
+    print(
+        json.loads(conversation_data.analysis.json())
+        .get("data_collection_results", {})
+        .get("consent_to_change_the_date", {})
+        .get("value", None)
+    )
+
+    # print(conversation_data.analysis.dict().get('evaluation_criteria_results').get('data_collection_results').get('consent_to_change_the_date').get('value'))
+
+    # # Extract data collection results
+    # data_collection = conversation_data.get("data_collection", {})
+    # # Access the consent_to_change_the_date boolean
+    # consent_to_change_the_date = data_collection.get("consent_to_change_the_date", False)
+
+    # print(f"Consent to change the date: {consent_to_change_the_date}")
+
 except Exception as e:
     print(f"Error: {e}")
     conversation.end_session()
