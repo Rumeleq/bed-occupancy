39843d8f7b49691f9ee5f51e255ae49dac74f29d
Wiktor Kycia <wiktor.kycia@gmail.com>
2025-05-15 10:02:38 +0200

    remove unused commented code, add elevenlabs to requirements

--- agent.py
+++ agent.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 from elevenlabs.client import ElevenLabs
 from elevenlabs.conversational_ai.conversation import Conversation, ConversationInitiationData
 from elevenlabs.conversational_ai.default_audio_interface import DefaultAudioInterface
-from elevenlabs.core import RequestOptions
 
 load_dotenv()
 
@@ -62,19 +61,7 @@ try:
     print(f"Conversation ID: {conversation_id}")
 
     # Retrieve conversation data using the conversation ID
-    # conversation_data = client.conversational_ai.get_conversation_data(conversation_id=conversation_id)
     conversation_data = client.conversational_ai.get_conversation(conversation_id=conversation_id)
-    # print(conversation_data.analysis.data_collection_results['consent_to_change_the_date'])
-
-    # with open("conversation.json", "w") as conversation_file:
-    #     conversation_file.write(conversation_data.analysis.json())
-    #
-    # with open("conversation.json", "r") as file:
-    #     data = json.load(file)
-    #
-    # consent_value = (data.get("data_collection_results", { })
-    #                  .get("consent_to_change_the_date", { })
-    #                  .get("value", None))
 
     print(
         json.loads(conversation_data.analysis.json())
@@ -82,15 +69,6 @@ try:
         .get("consent_to_change_the_date", {})
         .get("value", None)
     )
-
-    # print(conversation_data.analysis.dict().get('evaluation_criteria_results').get('data_collection_results').get('consent_to_change_the_date').get('value'))
-
-    # # Extract data collection results
-    # data_collection = conversation_data.get("data_collection", {})
-    # # Access the consent_to_change_the_date boolean
-    # consent_to_change_the_date = data_collection.get("consent_to_change_the_date", False)
-
-    # print(f"Consent to change the date: {consent_to_change_the_date}")
 
 except Exception as e:
     print(f"Error: {e}")
